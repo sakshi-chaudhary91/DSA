e980b1a76cdeec74c122f3366e0ef721e12bee97
sakshi-chaudhary91 <sakshichaudhary9140@gmail.com>
2026-07-01 02:04:40 +0530

    Create 1. Largest_Element.java

---
+++ Arrays/1. Largest_Element.java
@@ -0,0 +1,11 @@
+/*
+Question: Largest Element in an Array
+Platform: LeetCode
+Topic: Arrays
+*/
+
+public class LargestElement {
+    public static void main(String[] args) {
+        // Your solution
+    }
+}
